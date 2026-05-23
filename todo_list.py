e8cc15727003c608bd7c271d000366b4546c6d3e
samah <samahhassan646@gmail.com>
2026-05-23 03:00:40 +0300

    real update

--- todo_list.py
+++ todo_list.py
@@ -1,1 +1,18 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class TodoTask(models.Model):
+    _name = 'todo.task'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'To Do Task'
+
+    active = fields.Boolean(default=True)
+    ref = fields.Char(default='new', readonly=True)
+    name = fields.Char(string="Name", required=True ,translate=True)
+    assign_to_id = fields.Many2one('res.partner')
+    description = fields.Text(string="Description" ,translate=True)
+    due_date = fields.Date(string="Due Date")
+    estimated_time = fields.Float(string="Estimated Time (Hours)")
+
 
